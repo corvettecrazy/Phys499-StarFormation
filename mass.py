8c0a1dac3225200a0b32aa9caf8d475527e1f3e2
corvettecrazy <corvettecrazy_@hotmail.com>
2017-03-06 17:38:41 +0000

    updated, still length problem

--- mass.py
+++ mass.py
@@ -7,9 +7,9 @@ t2=Table.read('cohrs_ultimatecatalog5.fits')
 
 valuecolumn = Column(np.zeros(len(t1)), name='Clump Mass')
 
-distance=t2['distance']
-flux=t1['flux']
+distance=np.array(t2['distance'])
+flux=np.array(t1['flux'])
 
 #Problem: length of distance and flux is not the same
 
-#mass=13.1*flux*distance**2 need to integrate a for loop
+mass=13.1*flux*distance**2 
